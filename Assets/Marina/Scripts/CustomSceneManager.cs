fd0251ff808accf5d93415a65756851ff24dd990
Marina <140115877+mhmarina@users.noreply.github.com>
2025-07-23 19:19:30 -0700

    Title scene + Scene mgr

---
+++ Assets/Marina/Scripts/CustomSceneManager.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CustomSceneManager : MonoBehaviour
+{
+    // Singleton
+    public static CustomSceneManager Instance;
+
+    void Awake()
+    {
+        if (!Instance)
+        {
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(this);
+            return;
+        }
+        DontDestroyOnLoad(this);
+    }
+
+    public static void PlayNextScene()
+    {
+        if(SceneManager.GetActiveScene().buildIndex < SceneManager.sceneCount)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+
+    public static void PlayPrevScene()
+    {
+        if (SceneManager.GetActiveScene().buildIndex > 0)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        }
+    }
+
+    public static void PlaySceneByIndex(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+
+    public static void PlaySceneByName(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+}
